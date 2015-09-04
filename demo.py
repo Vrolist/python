3a5269662f12205fcbd108be1d7fbe093555ae12
Vrolist <kellyhu112@qq.com>
2015-09-04 14:26:32 +0800

    rm pygame folder and war file

--- demo.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 26 23:38:53 2015
-
-@author: hus
-"""
-
--- pygame/ball.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-#Filename:ball.py
-
-import pygame
-from pygame.locals import *
-
